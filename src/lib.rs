075d6ecad1f86e4391268959d22d5b0c72703989
elysia <3302843873@qq.com>
2026-05-09 12:03:02 +0800

    added lib.rs docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,90 @@
+//! # os-test-framework
+//!
+//! Test framework for embedded systems and OS kernels.
+//!
+//! ## Getting Started
+//!
+//! First, enable `custom_test_frameworks`, set `test_runner` as the test runner
+//! from `os-test-framework`, and `reexport_test_harness_main`.
+//!
+//! ```rust,ignore
+//! #![feature(custom_test_frameworks)]
+//! #![reexport_test_harness_main = "test_main"]
+//! #![test_runner(os_test_framework::run_tests)]
+//!
+//! use core::fmt::Write;
+//!
+//! use os_test_framework::{
+//!     platform::{ExitState, Platform, init_platform},
+//!     test,
+//! };
+//! ```
+//!
+//! Implement `Platform`. The framework writes output through
+//! `core::fmt::Write` and finishes the run through `Platform::exit`:
+//!
+//! ```rust,ignore
+//! struct MyPlatform;
+//!
+//! impl Platform for MyPlatform {
+//!     fn exit(&self, state: ExitState) -> ! {
+//!         match state {
+//!             ExitState::Success => todo!(),
+//!             ExitState::Failed => todo!(),
+//!         }
+//!     }
+//! }
+//!
+//! impl Write for MyPlatform {
+//!     fn write_str(&mut self, s: &str) -> core::fmt::Result {
+//!         // Write to serial, frame buffer, UART, etc.
+//!         let _ = s;
+//!         todo!()
+//!     }
+//! }
+//! ```
+//!
+//! Call `init_platform` with your `Platform`, and `test_main` from your kernel
+//! entry point:
+//!
+//! ```rust,ignore
+//! fn kernel_entry() {
+//!     init_platform(MyPlatform);
+//!     test_main();
+//! }
+//! ```
+//!
+//! ## Adding A Test
+//!
+//! You can declare tests like this:
+//!
+//! ```rust,ignore
+//! use os_test_framework::test;
+//!
+//! test! {
+//!     "Hello" {
+//!         assert!(true);
+//!     }
+//! }
+//! ```
+//!
+//! You can add multiple tests in the same file:
+//!
+//! ```rust,ignore
+//! use os_test_framework::test;
+//!
+//! test! {
+//!     "Hello" {
+//!         assert!(true);
+//!     }
+//! }
+//!
+//! test! {
+//!     "Hello2" {
+//!         assert!(true);
+//!     }
+//! }
+//! ```
 #![no_std]
 
 use owo_colors::OwoColorize;
