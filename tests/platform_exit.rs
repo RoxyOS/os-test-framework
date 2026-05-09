b7380a69cec930fec52d1b4d7f389ef729dafd05
elysia <3302843873@qq.com>
2026-05-09 11:33:11 +0800

    added platform exit test

---
+++ tests/platform_exit.rs
@@ -0,0 +1,40 @@
+#![feature(custom_test_frameworks)]
+#![test_runner(test_runner)]
+
+use core::panic;
+use std::{fmt::Write, process::exit};
+
+use os_test_framework::{
+    platform::{ExitState, Platform, init_platform, platform},
+    test,
+};
+
+struct TestPlatform;
+
+impl Platform for TestPlatform {
+    fn exit(&self, state: os_test_framework::platform::ExitState) -> ! {
+        match state {
+            ExitState::Success => panic!("Test shouldent succeed"),
+            ExitState::Failed => exit(0),
+        }
+    }
+}
+
+impl Write for TestPlatform {
+    fn write_str(&mut self, s: &str) -> std::fmt::Result {
+        print!("{s}");
+        Ok(())
+    }
+}
+
+fn test_runner(tests: &[&dyn Fn()]) {
+    init_platform(TestPlatform);
+    os_test_framework::run_tests(tests);
+}
+
+test! {
+    "Platform exit" {
+        println!();
+        platform().lock().exit(ExitState::Failed)
+    }
+}
