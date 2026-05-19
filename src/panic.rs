fb06231777dc6351b11093b530145a0644c3b844
elysia <3302843873@qq.com>
2026-05-19 08:38:10 +0800

    added forward_panic macro

--- src/panic.rs
+++ src/panic.rs
@@ -7,11 +7,25 @@ use crate::platform::{ExitState, try_platform};
 pub fn handle_panic(panic_info: &PanicInfo) -> ! {
     if let Some(platform) = try_platform() {
         let mut platform = platform.lock();
-        platform.print(format_args!(" {}\n\n{}\n", "Failed".bright_red().bold(), panic_info.message().red()));
+        platform.print(format_args!(
+            " {}\n\n{}\n",
+            "Failed".bright_red().bold(),
+            panic_info.message().red()
+        ));
         platform.exit(ExitState::Failed)
     }
 
     loop {
         core::hint::spin_loop();
     }
+}
+
+#[macro_export]
+macro_rules! forward_panic {
+    () => {
+        #[panic_handler]
+        fn _os_test_framework_panic_handler(panic_info: &core::panic::PanicInfo) -> ! {
+            $crate::handle_panic(panic_info);
+        }
+    };
 }
